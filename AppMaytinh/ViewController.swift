5eb1e39051608e8e0127628fa423af754a36b8db
datgvtdev03 <datgvtdev03@gmail.com>
2023-04-03 22:37:28 +0700

    Initial Commit

---
+++ AppMaytinh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMaytinh
+//
+//  Created by Thanh Dat on 03/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
